30c498395b929e7f2d3f6e336fca5ad86a69bf74
Sailu Goolawar <78556643+SailuGoolawar@users.noreply.github.com>
2024-06-03 15:56:34 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,10 +1,12 @@
 function formatCurrency(number) {
     if (number >= 1000 && number < 1000000) {
-        return (number / 1000).toFixed(1) + 'K';
+        return Math.floor(number / 100) / 10 + 'K';
     } else if (number >= 1000000 && number < 1000000000) {
-        return (number / 1000000).toFixed(1) + 'M';
-    } else if (number >= 1000000000) {
-        return (number / 1000000000).toFixed(1) + 'B';
+        return Math.floor(number / 100000) / 10 + 'M';
+    } else if (number >= 1000000000 && number < 1000000000000) {
+        return Math.floor(number / 100000000) / 10 + 'B';
+    } else if (number >= 1000000000000) {
+        return (number / 1000000000000).toFixed(1) + 'T';
     } else {
         return number.toString();
     }
