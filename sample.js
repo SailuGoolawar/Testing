e7401806cfcf974138bca8e8b009b47fba3f3911
Sailu Goolawar <78556643+SailuGoolawar@users.noreply.github.com>
2024-07-07 23:24:03 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,1 +1,12 @@
 
+jest.mock('shared-ui/src/lib/RiskAccordion', () => ({
+  RiskAccordion: jest.fn(({ accData }) => (
+    <div>
+      {accData.map((item) => (
+        <div key={item.accTitle} onClick={() => item.onTitleClick(item.accTitle)}>
+          {item.accTitle}
+        </div>
+      ))}
+    </div>
+  )),
+}));
